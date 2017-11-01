bee040b2d145638a219138683945d47105e44aac
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2017-11-01 18:12:14 +0200

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+(function () {
+	'use strict';
+
+	if (typeof Prism === 'undefined') {
+		console.log('Prism not defined');
+		return false;
+	}
+
+	if (!Prism.languages.markup || !Prism.languages.javascript) {
+		console.log('prism-ejs-language required markup and javascript languages');
+		return false;
+	}
+
+	Prism.languages.ejs = Prism.languages.extend('markup', {
+		'comment': /(<%%?#)[\s\S]*?([-]?%%?>)/g,
+		'punctuation': /(<%%?[-|=|_]?|[-|_]?%%?>)/g
+	});
+
+	Prism.languages.insertBefore('ejs', 'tag', {
+		script: {
+			pattern: /(<%%?[-|=|_]?)[\s\S]*?(?=[-|_]?%%?>)/i,
+			lookbehind: !0,
+			inside: Prism.languages.javascript,
+			alias: 'language-javascript'
+		}
+	});
+}());
